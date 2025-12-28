2208d43a82187c3f574c1f1a7a5389292536fccb
mustafa-raza-26 <mustafa778823@gmail.com>
2025-12-28 12:30:33 +0500

    Update

--- app1.js
+++ app1.js
@@ -1,40 +1,62 @@
 let logEmail = document.getElementById('logEmail');
 let logPass = document.getElementById('logPass');
 let loginBtn = document.getElementById('loginBtn');
+let logOut = document.getElementById('logout');
+let loginStatus = true;
 
 if (loginBtn) {
     loginBtn.addEventListener('click', async () => {
         const { data, error } = await client.auth.signInWithPassword({
-        email: logEmail.value,
-        password: logPass.value,
-        })
+            email: logEmail.value,
+            password: logPass.value,
+        });
         if (error) {
             console.log('errorLogin', error.message);
-        }else{
+        } else {
             console.log('dataLogin', data);
-            alert('You login')
-            window.location.href = '/dashboard.html'
-        }        
-    })
+            localStorage.setItem('login', true);
+            alert('You logged in');
+            window.location.href = '/dashboard.html';
+        }
+    });
+}
+
+if (window.location.pathname == '/index.html') {
+    const loggedIn = JSON.parse(localStorage.getItem('login'));
+    if (loggedIn) {
+        window.location.href = '/dashboard.html';
+    }
 }
 
 if (window.location.pathname == '/dashboard.html') {
     window.onload = async () => {
-        const { data, error } = await client.auth.getSession()
-        if (!data.session) {
-            window.location.href = '/index.html'
+        const { data, error } = await client.auth.getSession();
+        if (error) {
+            console.log('Error fetching session:', error.message);
+        } else if (!data.session) {
+            window.location.href = '/index.html';
         }
-    }
+    };
 }
-
 
 if (window.location.pathname == '/profile.html') {
     window.onload = async () => {
-        const { data, error } = await client.auth.getSession()
-        if (!data.session) {
-            window.location.href = '/index.html'
+        const { data, error } = await client.auth.getSession();
+        if (error) {
+            console.log('Error fetching session:', error.message);
+        } else if (!data.session) {
+            window.location.href = '/index.html';
         }
-        let id = data.session.user.user_metadata.first_name
-        display.innerText = `Name: ${id}`
-    }
+        if (data.session) {
+            let id = data.session.user.user_metadata.first_name;
+            display.innerText = `Name: ${id}`;
+        }
+    };
+}
+
+if (logOut) {
+    logOut.addEventListener('click', async () => {
+        localStorage.removeItem('login');
+        window.location.href = '/index.html'; 
+    });
 }
